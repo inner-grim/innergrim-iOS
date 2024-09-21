9c04aee09f700c9aec1eedf4fa50d268d04d1a39
noeyiz <lamoureclatant@gmail.com>
2024-09-21 14:52:35 +0900

    feat: 프로젝트 생성

---
+++ innergrim/innergrim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  innergrim
+//
+//  Created by 지연 on 9/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
