94f430d49f2c82be8319dda1b8a193356f6e6e91
noeyiz <lamoureclatant@gmail.com>
2024-10-10 18:07:10 +0900

    feat: LoginView UI 구현
    - 소셜 로그인(카카오, 애플, 구글) 버튼 배치

--- Projects/Feature/Onboarding/Sources/Login/LoginView.swift
+++ Projects/Feature/Onboarding/Sources/Login/LoginView.swift
@@ -6,17 +6,64 @@
 //
 
 import UIKit
+import SnapKit
+
+import Shared
 
 public final class LoginView: UIView {
+    // MARK: - UI Components
+    
+    lazy var kakaoLoginButton = createButton("카카오")
+    
+    lazy var appleLoginButton = createButton("애플")
+    
+    lazy var googleLoginButton = createButton("구글")
+    
+    private let buttonContainer = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        return stackView
+    }()
+    
     // MARK: - Initialize
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemBackground
+        setupUI()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    
+    private func setupUI() {
+        backgroundColor = .systemBackground
+        
+        addSubview(buttonContainer)
+        buttonContainer.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(42)
+            make.height.equalTo(184) // 버튼 높이: 56, 간격: 8
+        }
+        
+        buttonContainer.addArrangedSubview(kakaoLoginButton)
+        buttonContainer.addArrangedSubview(appleLoginButton)
+        buttonContainer.addArrangedSubview(googleLoginButton)
+    }
+}
+
+private extension LoginView {
+    func createButton(_ title: String) -> UIButton {
+        let button = UIButton()
+        button.configuration = .tinted()
+        button.configuration?.title = title
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        return button
     }
 }
