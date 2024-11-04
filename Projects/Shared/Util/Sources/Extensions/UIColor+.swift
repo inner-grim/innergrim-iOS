b93cfc8ec5339ac71e79b9917c8a911eccfb76f6
noeyiz <lamoureclatant@gmail.com>
2024-11-05 06:08:09 +0900

    feat: 로그인 관련 Image Assets 추가
    - UIImage extension으로 이미지 정의

--- Projects/Shared/Util/Sources/Extensions/UIColor+.swift
+++ Projects/Shared/Util/Sources/Extensions/UIColor+.swift
@@ -11,146 +11,154 @@ import SharedDesignSystem
 
 extension UIColor {
     public static var coral50: UIColor {
-        SharedDesignSystemAsset.coral50.color
+        SharedDesignSystemAsset.Colors.coral50.color
     }
     
     public static var coral100: UIColor {
-        SharedDesignSystemAsset.coral100.color
+        SharedDesignSystemAsset.Colors.coral100.color
     }
     
     public static var coral200: UIColor {
-        SharedDesignSystemAsset.coral200.color
+        SharedDesignSystemAsset.Colors.coral200.color
     }
     
     public static var coral300: UIColor {
-        SharedDesignSystemAsset.coral300.color
+        SharedDesignSystemAsset.Colors.coral300.color
     }
     
     public static var coral400: UIColor {
-        SharedDesignSystemAsset.coral400.color
+        SharedDesignSystemAsset.Colors.coral400.color
     }
     
     public static var coral500: UIColor {
-        SharedDesignSystemAsset.coral500.color
+        SharedDesignSystemAsset.Colors.coral500.color
     }
     
     public static var coral600: UIColor {
-        SharedDesignSystemAsset.coral600.color
+        SharedDesignSystemAsset.Colors.coral600.color
     }
     
     public static var coral700: UIColor {
-        SharedDesignSystemAsset.coral700.color
+        SharedDesignSystemAsset.Colors.coral700.color
     }
     
     public static var coral800: UIColor {
-        SharedDesignSystemAsset.coral800.color
+        SharedDesignSystemAsset.Colors.coral800.color
     }
     
     public static var coral900: UIColor {
-        SharedDesignSystemAsset.coral900.color
+        SharedDesignSystemAsset.Colors.coral900.color
     }
     
     public static var disabled: UIColor {
-        SharedDesignSystemAsset.disabled.color
+        SharedDesignSystemAsset.Colors.disabled.color
     }
     
     public static var fillAssistive: UIColor {
-        SharedDesignSystemAsset.fillAssistive.color
+        SharedDesignSystemAsset.Colors.fillAssistive.color
     }
     
     public static var fillDisabled: UIColor {
-        SharedDesignSystemAsset.fillDisabled.color
+        SharedDesignSystemAsset.Colors.fillDisabled.color
     }
     
     public static var fillNormal: UIColor {
-        SharedDesignSystemAsset.fillNormal.color
+        SharedDesignSystemAsset.Colors.fillNormal.color
     }
     
     public static var gray100: UIColor {
-        SharedDesignSystemAsset.gray100.color
+        SharedDesignSystemAsset.Colors.gray100.color
     }
     
     public static var gray200: UIColor {
-        SharedDesignSystemAsset.gray200.color
+        SharedDesignSystemAsset.Colors.gray200.color
     }
     
     public static var gray300: UIColor {
-        SharedDesignSystemAsset.gray300.color
+        SharedDesignSystemAsset.Colors.gray300.color
     }
     
     public static var gray400: UIColor {
-        SharedDesignSystemAsset.gray400.color
+        SharedDesignSystemAsset.Colors.gray400.color
     }
     
     public static var gray500: UIColor {
-        SharedDesignSystemAsset.gray500.color
+        SharedDesignSystemAsset.Colors.gray500.color
     }
     
     public static var gray600: UIColor {
-        SharedDesignSystemAsset.gray600.color
+        SharedDesignSystemAsset.Colors.gray600.color
     }
     
     public static var gray650: UIColor {
-        SharedDesignSystemAsset.gray650.color
+        SharedDesignSystemAsset.Colors.gray650.color
     }
     
     public static var gray700: UIColor {
-        SharedDesignSystemAsset.gray700.color
+        SharedDesignSystemAsset.Colors.gray700.color
     }
     
     public static var gray750: UIColor {
-        SharedDesignSystemAsset.gray750.color
+        SharedDesignSystemAsset.Colors.gray750.color
     }
     
     public static var gray800: UIColor {
-        SharedDesignSystemAsset.gray800.color
+        SharedDesignSystemAsset.Colors.gray800.color
     }
     
     public static var gray850: UIColor {
-        SharedDesignSystemAsset.gray850.color
+        SharedDesignSystemAsset.Colors.gray850.color
     }
     
     public static var gray900: UIColor {
-        SharedDesignSystemAsset.gray900.color
+        SharedDesignSystemAsset.Colors.gray900.color
+    }
+    
+    public static var kakaoFill: UIColor {
+        SharedDesignSystemAsset.Colors.kakaoFill.color
+    }
+    
+    public static var kakaoLabel: UIColor {
+        SharedDesignSystemAsset.Colors.kakaoLabel.color
     }
     
     public static var labelAssistive: UIColor {
-        SharedDesignSystemAsset.labelAssistive.color
+        SharedDesignSystemAsset.Colors.labelAssistive.color
     }
     
     public static var labelDisabled: UIColor {
-        SharedDesignSystemAsset.labelDisabled.color
+        SharedDesignSystemAsset.Colors.labelDisabled.color
     }
     
     public static var labelNeutral: UIColor {
-        SharedDesignSystemAsset.labelNeutral.color
+        SharedDesignSystemAsset.Colors.labelNeutral.color
     }
     
     public static var labelNormal: UIColor {
-        SharedDesignSystemAsset.labelNormal.color
+        SharedDesignSystemAsset.Colors.labelNormal.color
     }
     
     public static var lineLight: UIColor {
-        SharedDesignSystemAsset.lineLight.color
+        SharedDesignSystemAsset.Colors.lineLight.color
     }
     
     public static var lineNormal: UIColor {
-        SharedDesignSystemAsset.lineNormal.color
+        SharedDesignSystemAsset.Colors.lineNormal.color
     }
     
     public static var lineStrong: UIColor {
-        SharedDesignSystemAsset.lineStrong.color
+        SharedDesignSystemAsset.Colors.lineStrong.color
     }
     
     public static var primaryLight: UIColor {
-        SharedDesignSystemAsset.primaryLight.color
+        SharedDesignSystemAsset.Colors.primaryLight.color
     }
     
     public static var primaryNormal: UIColor {
-        SharedDesignSystemAsset.primaryNormal.color
+        SharedDesignSystemAsset.Colors.primaryNormal.color
     }
     
     public static var primaryStrong: UIColor {
-        SharedDesignSystemAsset.primaryStrong.color
+        SharedDesignSystemAsset.Colors.primaryStrong.color
     }
 }
---
+++ Projects/Shared/Util/Sources/Extensions/UIImage+.swift
@@ -0,0 +1,24 @@
+//
+//  UIImage+.swift
+//  SharedUtil
+//
+//  Created by 지연 on 11/5/24.
+//
+
+import UIKit
+
+import SharedDesignSystem
+
+extension UIImage {
+    public static var apple: UIImage {
+        SharedDesignSystemAsset.Images.apple.image
+    }
+    
+    public static var google: UIImage {
+        SharedDesignSystemAsset.Images.google.image
+    }
+    
+    public static var kakao: UIImage {
+        SharedDesignSystemAsset.Images.kakao.image
+    }
+}
